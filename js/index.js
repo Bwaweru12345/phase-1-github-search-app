8f3de0a5cee0d0364f849ea10d21d98e79e12451
Bwaweru12345 <bryanwaweru1076@example.com>
2023-04-03 15:39:18 +0300

    Complete assignment

--- js/index.js
+++ js/index.js
@@ -1,51 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form')
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        const users = form.search.value
-        getGitUser(users)
-        form.reset()
-    })
-})
-function getGitUser(username){
-    fetch(`https://api.github.com/search/users?q=${username}`, {method: 'GET', headers: {
-        Accept: 'application/vnd.github.v3+json'
-    }}).then(res => res.json()).then(data => {
-        const users = data.items
-        users.forEach(user => {
-            userList(user)
-        });
-    })
-}
-function getUserRepos(url){
-    fetch(url, {method: 'GET', headers: {
-        Accept: 'application/vnd.github.v3+json'
-    }}).then(res => res.json()).then(data => userRepos(data))
-}
-function userList(user){
-    const userUl = document.getElementById('user-list')
-    const userLi = document.createElement('li')
-    userLi.style.padding = '15px'
-    userLi.innerHTML =
-    `
-    <img src='${user.avatar_url}' width='100' height='100' alt='avatar' />
-    <h2 id='${user.login}'>${user.login}</h2>
-    `
-    userUl.appendChild(userLi)
-    document.getElementById(`${user.login}`).addEventListener('click', () => {
-        const repoUrl = `${user.repos_url}`
-        getUserRepos(repoUrl)
-    })
-}
-function userRepos(repos){
-    const reposUl = document.getElementById('repos-list')
-    repos.map(repo => {
-        const reposLi = document.createElement('li')
-        reposLi.textContent = repo.name
-        console.log(repo.name);
-        reposUl.appendChild(reposLi)
-    })
-}
-// document.addEventListener("DOMContentLoaded", () => {
- 
+// Begin of code
 
+const userList=document.getElementById('user-list')
+let repoList=document.getElementById('repos-list')
+
+
+const form=document.querySelector('#github-form')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let username=e.target.search.value
+    console.log('searching github')
+    fetch(`https://api.github.com/search/users?q=${username}`)
+    .then(res=>res.json())
+    .then(data=>data.forEach(user=>{
+
+      let h5=document.createElement('h5')  
+      h5.innerText =`${user.login}`
+      let img=document.createElement('img')  
+      img.src=`{user.avatar}`
+      let link=document.createElement('a')
+      link.href=`link to their  profile`
+
+      
+      
+      //add event to the users to show more info about them
+      h5.addEventListener('click',(e)=>{
+        let username=e.target.value//get the username
+
+        //make a fetch request with the returned value
+        //display the returned results on the DOM
+        fetch(`https://api.github.com/users/${username}/repos`)
+        .then(res=>res.json())
+        .then(repos=>repos.forEach(repo=>{
+         let repo=document.createElement('li')
+         repo.innerText=`${repo.name}`
+
+        }))
+        repoList.append(repo)
+      });
+      userList.appendChild(h5,img,link)
+    })
+          
+)
+})
